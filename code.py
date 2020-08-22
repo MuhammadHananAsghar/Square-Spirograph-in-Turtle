17abfe132a488f314ae998620012ee0a17e4d46e
Muhammad Hanan Asghar <44013285+MuhammadHananAsghar@users.noreply.github.com>
2020-08-22 21:57:27 +0545

    Create code.py

---
+++ code.py
@@ -0,0 +1,18 @@
+# CREATED BY MUHAMMAD HANAN ASGHAR
+import turtle
+turtle.bgcolor('black')
+turtle.pensize(3)
+
+
+for _ in range(5):
+	for color in ["red","blue","yellow","magenta","cyan","white","green"]:
+		turtle.color(color)
+		turtle.left(12)
+		turtle.forward(200)
+		turtle.left(90)
+		turtle.forward(200)
+		turtle.left(90)
+		turtle.forward(200)
+		turtle.left(90)
+		turtle.forward(200)
+		turtle.left(90)
